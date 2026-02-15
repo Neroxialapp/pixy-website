9495704706db10f47304fea7ccdd2c8cb183e3bb
Neroxialapp <63022455+Neroxialapp@users.noreply.github.com>
2026-02-15 22:14:53 +0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,10 @@
+import { createRoot } from "react-dom/client";
+import App from "./app/App.tsx";
+// Buradaki standart import kalsın ama Vite config ile bunu JS içine gömeceğiz
+import "./styles/index.css";
 
-  import { createRoot } from "react-dom/client";
-  import App from "./app/App.tsx";
-  import "./styles/index.css";
+const rootElement = document.getElementById("root");
 
-  createRoot(document.getElementById("root")!).render(<App />);
-  
+if (rootElement) {
+  createRoot(rootElement).render(<App />);
+}
